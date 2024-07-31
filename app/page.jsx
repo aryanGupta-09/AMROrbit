d0199a342b101b7b760dafee50dec54351f5ac96
Aryan Gupta <96881807+aryanGupta-09@users.noreply.github.com>
2024-07-31 22:33:15 +0530

    added antibiotic and organism options

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,18 @@
+"use client";
+import { Provider } from "react-redux";
+import { store } from "@/redux/store";
+import Options from "@/components/home/Options";
+
 export default function Home() {
   return (
-    <></>
+    <main>
+      <Provider store={store}>
+        <div className="mt-4 mb-4 text-center">
+          <h1 className="text-4xl">Vivli Dashboard</h1>
+          <p>A dashboard for visualizing antibiotic resistance data.</p>
+        </div>
+        <Options />
+      </Provider>
+    </main>
   );
 }
