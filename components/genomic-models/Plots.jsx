2a82649cd8574f7abfb0f750881facdf6f4997e5
Aryan Gupta <96881807+aryanGupta-09@users.noreply.github.com>
2024-08-22 21:01:49 +0530

    updated genomic card

--- components/genomic-models/Plots.jsx
+++ components/genomic-models/Plots.jsx
@@ -9,22 +9,22 @@ export default function BayesianNetworkPlots() {
     return (
         <div className="flex flex-wrap flex-row w-full my-10 justify-center gap-x-14 gap-y-10">
             <GenomicCard
-                image={<Image src={Ecoli_BSI} alt="E.Coli BSI" className="rounded-t-xl" />}
+                image={<Image src={Ecoli_BSI} alt="E.Coli BSI" className="rounded-t-xl h-full object-cover" />}
                 title="E.Coli BSI"
                 link="http://amrorbit.tavlab.iiitd.edu.in:3838/ecolibsi/customDashboard/inst/cd/"
             />
             <GenomicCard
-                image={<Image src={Ecoli_UTI} alt="E.Coli UTI" className="rounded-t-xl" />}
+                image={<Image src={Ecoli_UTI} alt="E.Coli UTI" className="rounded-t-xl h-full object-cover" />}
                 title="E.Coli UTI"
                 link="http://amrorbit.tavlab.iiitd.edu.in:3838/ecoliuti/customDashboard/inst/cd/"
             />
             <GenomicCard
-                image={<Image src={Klebsiella_Pneumoniae_BSI} alt="Klebsiella Pneumoniae BSI" className="rounded-t-xl" />}
+                image={<Image src={Klebsiella_Pneumoniae_BSI} alt="Klebsiella Pneumoniae BSI" className="rounded-t-xl h-full object-cover" />}
                 title="Klebsiella Pneumoniae BSI"
                 link="http://amrorbit.tavlab.iiitd.edu.in:3838/klebbsi/customDashboard/inst/cd/"
             />
             <GenomicCard
-                image={<Image src={Klebsiella_Pneumoniae_UTI} alt="Klebsiella Pneumoniae UTI" className="rounded-t-xl" />}
+                image={<Image src={Klebsiella_Pneumoniae_UTI} alt="Klebsiella Pneumoniae UTI" className="rounded-t-xl h-full object-cover" />}
                 title="Klebsiella Pneumoniae UTI"
                 link="http://amrorbit.tavlab.iiitd.edu.in:3838/klebuti/customDashboard/inst/cd/"
             />
