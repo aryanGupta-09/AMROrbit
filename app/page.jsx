4375a604f0471673998262fc83369bc819e5528b
Aryan Gupta <96881807+aryanGupta-09@users.noreply.github.com>
2024-08-22 20:56:08 +0530

    updated landing page data

--- app/page.jsx
+++ app/page.jsx
@@ -13,19 +13,19 @@ export default function Home() {
         <ExploreTab
           title="Scorecards"
           img={<Image src={landingScorecard} alt="Scorecard" className="rounded-l-xl" />}
-          description="Check out our Scorecards."
+          description="The Scorecard presents a phase-space plot with resistance levels on the x-axis and the rate of change on the y-axis. Countries in the bottom-left quadrant display favorable AMR profiles, characterized by low initial resistance and a slow increase over time. The remaining quadrants identify countries where targeted interventions are needed due to rising resistance, high baseline resistance, or both."
           link="/scorecards"
         />
         <ExploreTab
           title="Surrogate Indicators"
           img={<Image src={landingLeadLag} alt="Surrogate Indicators" className="rounded-l-xl" />}
-          description="Check out our Surrogate Indicators."
+          description="Cross-correlation analysis, including lead/lag analysis, was employed to assess the temporal relationships between resistance patterns across different antibiotics. These surrogate indicators act as early warning signs, offering valuable insights for timely intervention and strategic decision-making in combating antimicrobial resistance. Our study identified antibiotic coupling at the global level, which can be further mined to guide and optimize surveillance strategies."
           link="/surrogate-indicators"
         />
         <ExploreTab
           title="Genomic Models"
           img={<Image src={landingGenomic} alt="Genomic Models" className="rounded-l-xl" />}
-          description="Check out our Genomic Models."
+          description="Bayesian Networks (BNs) were utilized to model the conditional dependencies between antibiotic resistance patterns and genomic data. This approach allows for understanding the relationships between genetic factors and resistance."
           link="/genomic-models"
         />
       </div>
--- components/landing/ExploreTab.jsx
+++ components/landing/ExploreTab.jsx
@@ -4,17 +4,17 @@ import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 
 export default function ExploreTab({ title, img, description, link }) {
     return (
-        <div className="flex flex-row justify-center items-center mx-auto bg-[#384559] w-10/12 rounded-xl shadow-lg gap-x-4">
+        <div className="flex flex-row justify-center items-center mx-auto bg-[#384559] w-10/12 rounded-xl shadow-lg gap-x-4 2xl:gap-x-7">
             <div className="w-1/4 h-full">
                 {img}
             </div>
-            <div className="w-3/4 flex flex-col justify-between gap-y-9 h-full">
-                <div className="flex flex-col gap-y-2">
+            <div className="w-3/4 flex flex-col justify-between h-full">
+                <div className="flex flex-col gap-y-4">
                     <h1 className="text-xl 2xl:text-2xl 3xl:text-3xl text-white">{title}</h1>
                     <h2 className="2xl:text-lg 3xl:text-xl text-gray-300">{description}</h2>
                 </div>
                 <Link href={link}>
-                    <Button className="w-fit 2xl:text-lg 3xl:text-xl" sx={{
+                    <Button className="w-fit 2xl:text-lg 3xl:text-xl mt-4 2xl:mt-7" sx={{
                         color: "white",
                         backgroundColor: "#4F6077",
                         '&:hover': {
