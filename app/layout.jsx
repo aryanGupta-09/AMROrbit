fa351cf14afc52af61509e258123ca26d1b35af4
sc0rp10n-py <space.sc0rp10n.py@gmail.com>
2025-05-31 22:08:19 +0530

    fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,32 +1,36 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import './polyfills.js';
+import "./polyfills.js";
 
 import Navbar from "@/components/layout/Navbar";
 import { ToasterProvider } from "@/helper/ToasterProvider";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "AMROrbit | TavLab",
-  description: "",
+    title: "AMROrbit | TavLab",
+    description: "",
 };
 
 export default function RootLayout({ children }) {
-  return (
-
-    <html lang="en">
-      <head>
-        <title>{metadata.title}</title>
-        <link rel="icon" href="/images/tavlab-favicon.webp" type="image/webp" />
-      </head>
-      <body className={inter.className}>
-        <ToasterProvider />
-        <Navbar />
-        <main>
-          {children}
-        </main>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <title>{metadata.title}</title>
+                <link
+                    rel="icon"
+                    href="/images/tavlab-favicon.webp"
+                    type="image/webp"
+                />
+            </head>
+            <body className={inter.className}>
+                <ToasterProvider />
+                <Navbar />
+                <main>
+                    <Suspense>{children}</Suspense>
+                </main>
+            </body>
+        </html>
+    );
 }
